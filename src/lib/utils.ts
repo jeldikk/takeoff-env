3149b0705712a87170e0a20ffa9c727867b6763c
jeldikk <jeldi.kamal2013@gmail.com>
2021-05-30 15:43:41 +0530

    small changes for better code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,12 @@
 import { promises as fs } from "fs";
+import {resolve} from "./fs-helps"
 
-export async function isNodeProject(path: string): Promise<boolean>{
-    const fileList = await fs.readdir(path);
+export async function isNodeProject(...paths: string[]): Promise<boolean>{
+    const fileList = await fs.readdir(resolve(...paths));
     return fileList.includes('package.json')
 }
 
-export async function isNodeEnvironmentPresent(projectPath: string): Promise<boolean>{
-    const fileList = await fs.readdir(projectPath);
+export async function isNodeEnvironmentPresent(...paths: string[]): Promise<boolean>{
+    const fileList = await fs.readdir(resolve(...paths));
     return fileList.includes('node_modules')
 }
