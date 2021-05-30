7308c02e6a1f6af6441db1f2ea92ff3bb686b5bd
jeldikk <jeldi.kamal2013@gmail.com>
2021-05-30 15:45:13 +0530

    test suite for helper functions

--- tests/fs-helps.test.ts
+++ tests/fs-helps.test.ts
@@ -1,5 +1,15 @@
-describe("Testing fs helper functions", ()=>{
-    test("test1", ()=>{
-        expect(1).toBe(1)
+import {isDirectory} from "../src/lib/fs-helps"
+import * as path from 'path'
+
+describe("Testing fs-helper functions", ()=>{
+
+    test("invalid directory testing", async ()=>{
+        const isDir = await isDirectory(path.resolve(__dirname, './fs-helps.test.ts'));
+        expect(isDir).toBe(false);
+    })
+
+    test("valid directory testing", async ()=>{
+        const isDir = await isDirectory(__dirname);
+        expect(isDir).toBe(true);
     })
 })
